25c76d1223abde69fa3154bcf937a305a642c8b2
fabri-tech <fabriziotrevino@gmail.com>
2023-05-15 23:09:18 -0600

    revised the server.js

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
